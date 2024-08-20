8a2c5f52e5c53cf9dbdbafb627e34ba16a62ea03
Nhung <hongnhung16052003@gmail.com>
2024-08-20 17:30:19 +0700

    handle logic of the clear button

--- src/components/AccountItem/AccountItem.js
+++ src/components/AccountItem/AccountItem.js
@@ -2,17 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import styles from './AccountItem.module.scss'
 import classNames from 'classnames/bind'
 import { faCheckCircle, faEllipsis } from '@fortawesome/free-solid-svg-icons'
+import Image from '../Image'
 
 const cx = classNames.bind(styles)
 function AccountItem() {
   return (
     <div className={cx('wrapper')}>
       <div className={cx('user')}>
-        <img
+        <Image
           className={cx('avatar')}
           src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/7dc789b28318e5489cfeeb1d39b35969~c5_1080x1080.jpeg?lk3s=a5d48078&nonce=26492&refresh_token=0fc6d6166d2e6214408b88e508b6b631&x-expires=1724205600&x-signature=SZhb5nfbBqDbTOikpzNiosltsPY%3D&shp=a5d48078&shcp=81f88b70"
           alt="avatar"
-        ></img>
+        ></Image>
         <div className={cx('info')}>
           <h4 className={cx('name')}>
             <span>Nhung</span>
--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons'
 import classNames from 'classnames/bind'
@@ -11,18 +11,31 @@ import AccountItem from '~/components/AccountItem'
 
 const cx = classNames.bind(styles)
 function Search() {
-  const [searchResult, setSearchResult] = useState([])
+  const [searchValue, setSearchValue] = useState('')
+  const [searchResult, setSearchResult] = useState([1, 2, 3, 4])
+  const [showResult, setShowResult] = useState(true)
 
+  const inputRef = useRef()
   useEffect(() => {
-    setTimeout(() => {
-      setSearchResult([1, 2, 3])
-    }, 0)
+    // setTimeout(() => {
+    //   setSearchResult([1, 2, 3])
+    // }, 0)
   }, [])
+
+  const handleClear = () => {
+    setSearchValue('')
+    setSearchResult([])
+    inputRef.current.focus()
+  }
+
+  const handleHideResult = () => {
+    setShowResult(false)
+  }
 
   return (
     <Tippy
       interactive
-      visible={searchResult.length > 0}
+      visible={showResult && searchResult.length > 0}
       render={(attrs) => (
         <div className={cx('search-result')} tabIndex="-1" {...attrs}>
           <PopperWrapper>
@@ -34,15 +47,27 @@ function Search() {
           </PopperWrapper>
         </div>
       )}
+      onClickOutside={handleHideResult}
     >
       <div className={cx('search')}>
-        <input type="text" className={cx('search-input')} placeholder="Search" spellCheck="false"></input>
+        <input
+          ref={inputRef}
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          type="text"
+          className={cx('search-input')}
+          placeholder="Search"
+          spellCheck="false"
+          onFocus={()=> setShowResult(true)}
+        ></input>
         {/* Clear */}
-        <button className={cx('clear')}>
-          <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
-        </button>
+        {!!searchValue && (
+          <button className={cx('clear')} onClick={handleClear}>
+            <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+          </button>
+        )}
         {/* Loading */}
-        <FontAwesomeIcon icon={faSpinner} className={cx('loading')} />
+        {/* <FontAwesomeIcon icon={faSpinner} className={cx('loading')} /> */}
         <span className={cx('search-line')}></span>
         {/* Search Icon */}
         <button className={cx('search-btn')}>
