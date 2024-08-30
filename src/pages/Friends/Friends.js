9e39f03087cb839c4b2d23e7a458861d38897797
Nhung <hongnhung16052003@gmail.com>
2024-08-31 00:07:07 +0700

    build UI of the friends page

--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -1,5 +1,43 @@
+import { useEffect, useState } from 'react'
+import styles from './Friends.module.scss'
+import classNames from 'classnames/bind'
+import { getSuggestedUsers } from '~/services/getSuggestedUsers'
+import FriendList from '~/components/FriendList/FriendList'
+
+const cx = classNames.bind(styles)
+
 function Friends() {
-  return <h2>Friends page</h2>
+  const [page, setPage] = useState(1)
+  const [userData, setUserData] = useState([])
+
+  useEffect(() => {
+    async function fetchApi() {
+      const data = await getSuggestedUsers(page, 9)
+      if (data) {
+        setUserData((prev) => [...prev, ...data])
+      }
+    }
+    fetchApi()
+  }, [page])
+
+  const handleScroll = () => {
+    if (window.scrollY + window.innerHeight >= document.body.offsetHeight) {
+      setPage((prev) => prev + 1)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
+  return (
+    <div className={cx('wrapper')}>
+      <FriendList data={userData} />
+    </div>
+  )
 }
 
 export default Friends
