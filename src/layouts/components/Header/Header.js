ed07d27010ba997c0ddba68822e75bf8b30b5eed
Nhung <hongnhung16052003@gmail.com>
2024-09-07 18:03:32 +0700

    fix: link to profile of the user menu

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -19,7 +19,7 @@ import { ChartIcon, CoinIcon, DarkIcon, FeedbackIcon, HubIcon, LogoutIcon, Setti
 import Image from '~/components/Image'
 import config from '~/config'
 import { ThemeContext } from '~/components/Context/ThemeContext'
-import { useContext} from 'react'
+import { useContext, useMemo } from 'react'
 import { UserContext } from '~/components/Context/UserContext'
 import { AuthContext } from '~/components/Modal/AuthModalContext'
 
@@ -29,117 +29,123 @@ function Header() {
   const themeContext = useContext(ThemeContext)
   const authContext = useContext(AuthContext)
   const userContext = useContext(UserContext)
-  const isLogin = useContext(UserContext).isLogin
+  const isLogin = userContext.isLogin
+  const {avatar, nickname} = userContext.currentUser
   const navigate = useNavigate()
   // MENU ITEMS
-  const MENU_ITEMS = [
-    {
-      icon: <CreatorIcon />,
-      title: 'Creator tools',
-      children: {
+  const MENU_ITEMS = useMemo(
+    () => [
+      {
+        icon: <CreatorIcon />,
         title: 'Creator tools',
-        data: [
-          {
-            icon: <HubIcon />,
-            title: 'LIVE Creator Hub',
-            to: 'https://www.tiktok.com/live/creators/vi-VN/?enter_from=more&lang=en&region=VN',
-          },
-        ],
+        children: {
+          title: 'Creator tools',
+          data: [
+            {
+              icon: <HubIcon />,
+              title: 'LIVE Creator Hub',
+              to: 'https://www.tiktok.com/live/creators/vi-VN/?enter_from=more&lang=en&region=VN',
+            },
+          ],
+        },
       },
-    },
-    {
-      icon: <LanguageIcon />,
-      title: 'English',
-      children: {
-        title: 'Language',
-        data: [
-          {
-            type: 'language',
-            code: 'en',
-            title: 'English',
-          },
-          {
-            type: 'language',
-            code: 'vi',
-            title: 'Tiếng Việt',
-          },
-        ],
+      {
+        icon: <LanguageIcon />,
+        title: 'English',
+        children: {
+          title: 'Language',
+          data: [
+            {
+              type: 'language',
+              code: 'en',
+              title: 'English',
+            },
+            {
+              type: 'language',
+              code: 'vi',
+              title: 'Tiếng Việt',
+            },
+          ],
+        },
       },
-    },
-    {
-      icon: <FeedbackIcon />,
-      title: 'Feedback and help',
-      to: '/feedback',
-    },
-    {
-      icon: <DarkIcon />,
-      title: 'Dark Mode',
-      children: {
+      {
+        icon: <FeedbackIcon />,
+        title: 'Feedback and help',
+        to: '/feedback',
+      },
+      {
+        icon: <DarkIcon />,
         title: 'Dark Mode',
-        data: [
-          {
-            type: 'darkmode',
-            title: 'Dark Mode',
-            // icon: themeContext.theme ? <FontAwesomeIcon icon={faCheck} /> : <svg style={{ width: '24px' }}></svg>,
-            icon: <svg style={{ width: '24px' }}></svg>,
-          },
-          {
-            type: 'darkmode',
-            title: 'Light Mode',
-            // icon: !themeContext.theme ? <FontAwesomeIcon icon={faCheck} /> : <svg style={{ width: '24px' }}></svg>,
-            icon: <svg style={{ width: '24px' }}></svg>,
-          },
-        ],
+        children: {
+          title: 'Dark Mode',
+          data: [
+            {
+              type: 'darkmode',
+              title: 'Dark Mode',
+              // icon: themeContext.theme ? <FontAwesomeIcon icon={faCheck} /> : <svg style={{ width: '24px' }}></svg>,
+              icon: <svg style={{ width: '24px' }}></svg>,
+            },
+            {
+              type: 'darkmode',
+              title: 'Light Mode',
+              // icon: !themeContext.theme ? <FontAwesomeIcon icon={faCheck} /> : <svg style={{ width: '24px' }}></svg>,
+              icon: <svg style={{ width: '24px' }}></svg>,
+            },
+          ],
+        },
       },
-    },
-  ]
-
+    ],
+    [],
+  )
   // USER MENU
-  const USER_MENU = [
-    {
-      icon: <UserIcon />,
-      title: 'View Profile',
-      to: `/@${userContext.currentUser.nickname}`,
-    },
-    {
-      icon: <CoinIcon />,
-      title: 'Get Coins',
-      href: 'https://www.tiktok.com/coin?enter_from=web_main_nav',
-    },
-    {
-      icon: <CreatorIcon />,
-      title: 'Creator Tools',
-      children: {
-        title: 'Creator Tools',
-        data: [
-          {
-            icon: <ChartIcon />,
-            title: 'View Analytics',
-          },
-          {
-            icon: <StudioIcon />,
-            title: 'LIVE Studio',
-          },
-          {
-            icon: <HubIcon />,
-            title: 'LIVE Creator Hub',
-          },
-        ],
+  const USER_MENU = useMemo(
+    () => [
+      {
+        icon: <UserIcon />,
+        title: 'View Profile',
+        to: `/@${nickname}`,
       },
-    },
-    {
-      icon: <SettingIcon />,
-      title: 'Settings',
-      to: '/setting',
-    },
-    ...MENU_ITEMS.slice(1),
-    {
-      icon: <LogoutIcon />,
-      title: 'Logout',
-      type: 'logout',
-      separate: true,
-    },
-  ]
+      {
+        icon: <CoinIcon />,
+        title: 'Get Coins',
+        href: 'https://www.tiktok.com/coin?enter_from=web_main_nav',
+      },
+      {
+        icon: <CreatorIcon />,
+        title: 'Creator Tools',
+        children: {
+          title: 'Creator Tools',
+          data: [
+            {
+              icon: <ChartIcon />,
+              title: 'View Analytics',
+            },
+            {
+              icon: <StudioIcon />,
+              title: 'LIVE Studio',
+            },
+            {
+              icon: <HubIcon />,
+              title: 'LIVE Creator Hub',
+            },
+          ],
+        },
+      },
+      {
+        icon: <SettingIcon />,
+        title: 'Settings',
+        to: '/setting',
+      },
+      ...MENU_ITEMS.slice(1),
+      {
+        icon: <LogoutIcon />,
+        title: 'Logout',
+        type: 'logout',
+        separate: true,
+      },
+    ],
+    [nickname],
+  )
 
   // Handle select menu
   const handleMenuChange = (menuItem) => {
@@ -149,11 +155,11 @@ function Header() {
         break
       case 'logout':
         const isConfirm = window.confirm('Are you sure you want to log out?')
-       if (isConfirm) {
-         localStorage.removeItem('token');
-         localStorage.removeItem('isLogin')
-         userContext.toggleLogin();
-       }
+        if (isConfirm) {
+          localStorage.removeItem('token')
+          localStorage.removeItem('isLogin')
+          userContext.toggleLogin()
+        }
         break
       case 'darkmode':
         switch (menuItem.title) {
@@ -171,11 +177,10 @@ function Header() {
             menuItems[1] = menuItem
             if (!themeContext.theme) {
               themeContext.toggleTheme()
-               menuItem.icon = <FontAwesomeIcon icon={faCheck} />
+              menuItem.icon = <FontAwesomeIcon icon={faCheck} />
               if (menuItems[0]) {
-                 
-                 menuItems[0].icon = <svg style={{ width: '24px' }}></svg>
-               }
+                menuItems[0].icon = <svg style={{ width: '24px' }}></svg>
+              }
             }
             break
           default:
@@ -216,14 +221,16 @@ function Header() {
                   </button>
                 </Tippy>
               </div>
-              <Menu items={USER_MENU} onChange={handleMenuChange}>
-                <Image
-                  className={cx('avatar')}
-                  src={userContext.currentUser.avatar}
-                  alt={userContext.currentUser.nickname}
-                  // fallback="https://fullstack.edu.vn/assets/f8-icon-lV2rGpF0.png"
-                ></Image>
-              </Menu>
+              {nickname && (
+                <Menu items={USER_MENU} onChange={handleMenuChange}>
+                  <Image
+                    className={cx('avatar')}
+                    src={avatar}
+                    alt={nickname}
+                    // fallback="https://fullstack.edu.vn/assets/f8-icon-lV2rGpF0.png"
+                  ></Image>
+                </Menu>
+              )}
             </div>
           ) : (
             <>
